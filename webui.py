3e441ac9de24196fe79632fafbd5006f5cd5fdb6
lihua202406 <lihua202301@gmail.com>
2024-07-13 17:43:39 +0800

    Update webui.py

--- webui.py
+++ webui.py
@@ -166,7 +166,7 @@ def main():
                               outputs=[audio_output])
         mode_checkbox_group.change(fn=change_instruction, inputs=[mode_checkbox_group], outputs=[instruction_text])
     demo.queue(max_size=4, default_concurrency_limit=2)
-    demo.launch(server_port=args.port)
+    demo.launch(server_port=args.port,share=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -183,4 +183,3 @@ if __name__ == '__main__':
     prompt_sr, target_sr = 16000, 22050
     default_data = np.zeros(target_sr)
     main()
-  .launch(share=False)
